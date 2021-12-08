4de278fe8b371ff09a7f659a35807b02aeaf2123
williamrolando88 <william.rolando88@hotmail.com>
2021-12-08 17:20:29 -0500

    remove item first test

--- src/__test__/addItem.test.js
+++ src/__test__/addItem.test.js
@@ -5,22 +5,32 @@
 import createListItem from '../_createListItem';
 import ToDoList from '../_ToDoList';
 
-describe('add item', () => {
-  const list = new ToDoList();
+const list = new ToDoList();
+list.add('Wash the dishes');
+const activitiesContainer = document.createElement('ul');
+const listItem = createListItem(
+  list.array[0].description,
+  list.array[0].estatus,
+  list.array[0].index,
+);
+activitiesContainer.appendChild(listItem);
 
+describe('add item', () => {
   test('list is instance of ToDoList?', () => {
     expect(list).toBeInstanceOf(ToDoList);
   });
-
-  list.add('Wash the dishes');
 
   test('add new task to list', () => {
     expect(list.array[0].description).toBe('Wash the dishes');
   });
   test('adds li to DOM', () => {
-    const activitiesContainer = document.createElement('ul');
-    const listItem = createListItem(list.array[0].description, list.array[0].estatus, list.array[0].index);
-    activitiesContainer.appendChild(listItem);
     expect(activitiesContainer.childElementCount).toBe(1);
-  })
+  });
+});
+
+describe('remo item', () => {
+  test('remove from DOM', () => {
+    activitiesContainer.removeChild(activitiesContainer.firstChild);
+    expect(activitiesContainer.childElementCount).toBe(0);
+  });
 });
