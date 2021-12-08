ee7b3c2a6265a1898fa8b31e00aceb7cb5596375
John-Arboleda <johnjack0126@gmail.com>
2021-12-08 15:45:06 -0500

    Test for adding a new task to the list

--- src/__test__/addItem.test.js
+++ src/__test__/addItem.test.js
@@ -2,7 +2,14 @@ import ToDoList from '../_ToDoList';
 
 describe('add item', () => {
   const list = new ToDoList();
+
   test('list is instance of ToDoList?', () => {
     expect(list).toBeInstanceOf(ToDoList);
+  });
+  
+  list.add('Wash the dishes');
+
+  test('add new task to list', () => {
+    expect(list.array[0].description).toBe('Wash the dishes');
   });
 });
